71ae44e47cf2de0580208540c7d59073a3526cb3
Laboratório FIAP <labsfiap@fiap.com.br>
2025-04-15 10:01:36 -0300

    constructor Pizza, $, List<Pizza> pizzaData, List<MenuItem> pizzas _buildPizzas()

--- lib/menu_item.dart
+++ lib/menu_item.dart
@@ -1,42 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:pizza_menu/pizza_data.dart';
+
 
 class MenuItem extends StatelessWidget {
-  const MenuItem({super.key});
+  final Pizza pizza;
+  const MenuItem({super.key, required this.pizza});
 
   @override
   Widget build(BuildContext context) {
     return Card(
-            color: Color.fromARGB(255, 223, 220, 178),
+            color: const Color.fromARGB(255, 223, 220, 178),
             elevation: 0,
             child: Padding(
               padding: const EdgeInsets.all(8.0),
               child: Row(
                 children: [
-                  Image.asset(
-                    'images/focaccia.jpg',
-                    width: 100,
-                    height: 100,
+                  ColorFiltered(
+                    colorFilter: pizza.soldOut ?
+                    ColorFilter.mode(Colors.grey, BlendMode.saturation) : 
+                    ColorFilter.mode(Colors.transparent, BlendMode.multiply),
+                    child: Image.asset(
+                      'images/${pizza.photoName}',
+                      width: 100,
+                      height: 100,
+                    ),
                   ),
                   const SizedBox(
                     width: 16,
                   ),
-                  const Column(
+                   Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       Text(
-                        "Text 1",
+                        pizza.name,
                         style: TextStyle(
                             fontSize: 16, fontWeight: FontWeight.bold),
                       ),
                       SizedBox(height: 6),
                       Text(
-                        "Text 2",
+                        pizza.ingredients,
                         style: TextStyle(fontSize: 14, color: Colors.black87),
                       ),
                       SizedBox(height: 6),
                       Text(
-                        "Text 3",
-                        style: TextStyle(color: Colors.black45),
+                        pizza.soldOut ? "Sould Out " : "\$${pizza.price.toString()}",
+                        style: pizza.soldOut ? TextStyle(color: Colors.red) : TextStyle(color: Colors.green),
                       )
                     ],
                   ),
--- lib/pizza_menu.dart
+++ lib/pizza_menu.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:pizza_menu/menu_item.dart';
-
+import 'package:pizza_menu/pizza_data.dart';
 
 class PizzaMenu extends StatelessWidget {
   const PizzaMenu({super.key});
@@ -8,17 +8,42 @@ class PizzaMenu extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Pizza Menu'),
-      ),
-      body: Column(
-        children: [
-          MenuItem(),
-          MenuItem(),
-          MenuItem(),
-          MenuItem(),
-        ],
-      ),
-    );
+        appBar: AppBar(
+          title: const Text('Pizza Menu'),
+        ),
+        body: Column(
+          children: [
+            Expanded(
+              child: SingleChildScrollView(
+                child: Column(
+                  children: _buildPizzas(),
+                  //...pizzaData.map((p) => MenuItem(pizza: p))
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(14.0),
+              child: SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.yellow[600],
+                      textStyle:
+                          TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                    ),
+                    onPressed: () {},
+                    child: Text("Order Now!")),
+              ),
+            )
+          ],
+        ));
+  }
+
+  List<MenuItem> _buildPizzas() {
+    List<MenuItem> pizzas = [];
+    for (var p in pizzaData) {
+      pizzas.add(MenuItem(pizza: p));
+    }
+    return pizzas;
   }
 }
